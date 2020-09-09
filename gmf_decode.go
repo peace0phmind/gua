748e73c7bb67ec56eb53a536df83d5659325042a
longlongint <945412026@qq.com>
2020-09-09 18:23:21 +0800

    using gmf, decode h264, encode jpeg

--- gmf_decode.go
+++ gmf_decode.go
@@ -6,8 +6,10 @@ package gua
 */
 import "C"
 import (
+	"fmt"
 	"github.com/peace0phmind/gmf"
 	"log"
+	"os"
 	"syscall"
 	"unsafe"
 )
@@ -34,11 +36,13 @@ func init() {
 	}
 	decoder[gmf.AV_CODEC_ID_MPEG4] = codec
 
-	codec, err = gmf.FindEncoder(gmf.AV_CODEC_ID_MJPEG)
+	codec, err = gmf.FindEncoder("mjpeg")
 	if err != nil {
 		log.Println("unable to find mjpeg encode codec")
 		return
 	}
+
+	encoder = codec
 }
 
 //export on_decode_cb
@@ -55,10 +59,14 @@ func on_decode_cb(ps *C.ps_codec) {
 	var cc *gmf.CodecCtx
 	if cc = gmf.NewCodecCtx(codec); cc == nil {
 		log.Println("unable to create codec context")
+		return
 	}
+
+	defer gmf.Release(cc)
 
 	if err := cc.Open(nil); err != nil {
 		log.Printf("open codec ctx err: %v\n", err)
+		return
 	}
 
 	frame, ret := cc.Decode2(pkt)
@@ -75,4 +83,94 @@ func on_decode_cb(ps *C.ps_codec) {
 	}
 
 	log.Printf("%v\n", frame)
+
+	occ := gmf.NewCodecCtx(encoder)
+	defer gmf.Release(occ)
+
+	occ.SetPixFmt(gmf.AV_PIX_FMT_YUVJ420P).SetWidth(cc.Width()).SetHeight(cc.Height())
+	occ.SetTimeBase(gmf.AVR{Num: 1, Den: 1})
+
+	if err := occ.Open(nil); err != nil {
+		log.Printf("occ open error: %v\n", err)
+		return
+	}
+
+	outputCtx, err := gmf.NewOutputCtx("test.jpeg")
+	if err != nil {
+		log.Printf("output ctx error: %v\n", err)
+		return
+	}
+	defer outputCtx.Free()
+	outputCtx.Dump()
+
+	//fg, err := gmf.NewSimpleVideoGraph("crop=w=800:h=600:x=0:y=0", cc, occ)
+	//if err != nil {
+	//	log.Printf("SimpleVideoGraph error: %v\n", err)
+	//	return
+	//}
+	//
+	//var (
+	//	ff    []*gmf.Frame
+	//)
+	//
+	//if frame != nil {
+	//	if err := fg.AddFrame(frame, 0, 1); err != nil {
+	//		log.Fatalf("%s\n", err)
+	//	}
+	//	fg.Dump()
+	//	frame.Free()
+	//}
+	//
+	//if err := fg.AddFrame(frame, 0, 4); err != nil {
+	//	log.Fatalf("%s\n", err)
+	//}
+	//frame.Free()
+	//
+	//if ff, err = fg.GetFrame(); err != nil && len(ff) == 0 {
+	//	log.Printf("GetFrame() returned '%s', continue\n", err)
+	//	return
+	//}
+	//
+	//if len(ff) == 0 {
+	//	log.Println("GetFrame() length zero")
+	//	return
+	//}
+
+	// TODO remove when fix fiter graph issue, use ff
+	result := []*gmf.Frame{frame}
+	packets, err := occ.Encode(result, -1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range result {
+		f.Free()
+	}
+
+	for _, op := range packets {
+		writeFile(op.Data())
+		op.Free()
+	}
+}
+
+const format = "./%03d.jpeg"
+
+var fileCount int
+
+func writeFile(b []byte) {
+	name := fmt.Sprintf(format, fileCount)
+
+	fp, err := os.OpenFile(name, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("%s\n", err)
+	}
+
+	if n, err := fp.Write(b); err != nil {
+		log.Fatalf("%s\n", err)
+	} else {
+		log.Printf("%d bytes written to '%s'", n, name)
+	}
+
+	fp.Close()
+	fileCount++
 }
